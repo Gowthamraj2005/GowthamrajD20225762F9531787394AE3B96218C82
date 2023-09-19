b20696609f21921f28b3eb2b31a267cc27cf877b
Gowthamraj2005 <144456072+Gowthamraj2005@users.noreply.github.com>
2023-09-19 18:08:20 +0530

    Update main(2).py

--- UNIT-1 CHALLENGE/main(2).py
+++ UNIT-1 CHALLENGE/main(2).py
@@ -1,7 +1,7 @@
-year = 2000
-if (year % 400 == 0) and (year % 100 == 0):
-  print("{0} is a leap year".format(year))
-elif (year % 4 ==0) and (year % 100 != 0):
-  print("{0} is a leap year".format(year))
+year = int(input("Enter a year: "))
+
+# Check if it's a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
 else:
-  print("{0} is not a leap year".format(year))
+    print(year, "is not a leap year")
